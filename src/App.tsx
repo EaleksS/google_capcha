2d2d3e08f4c30144c6479475f1c074315db68bf4
EaleksS <ealgenv@gmail.com>
2023-05-10 23:25:14 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
+import { FC } from "react";
 import "./App.css";
-import {
-  GoogleReCaptcha,
-  GoogleReCaptchaProvider,
-} from "react-google-recaptcha-v3-non-autoload";
+import ReCAPTCHA from "react-google-recaptcha";
 
-function App() {
+const App: FC = () => {
   return (
     <>
-      <GoogleReCaptchaProvider reCaptchaKey="6LdrbvklAAAAAGMcyiX70N_J0QZGycnpDdTJYTtK">
-        <GoogleReCaptcha onVerify={(v) => console.log(v)} />
-      </GoogleReCaptchaProvider>
-      , ,
+      <ReCAPTCHA
+        sitekey="6LdrbvklAAAAAKc2anqIkJrTREXWKHiwZf0v6mAN"
+        onChange={(value) => console.log("Captcha value:", value)}
+      />
     </>
   );
-}
+};
 
 export default App;
