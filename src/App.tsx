bd0a3d4a9ca3222e8f1165f48f14ffdc5ad72572
EaleksS <ealgenv@gmail.com>
2023-05-17 23:01:27 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import "./App.css";
 import ReCAPTCHA from "react-google-recaptcha";
 
 const App: FC = () => {
+  const [captcha, setCaptcha] = useState<string | null>(null);
+
+  console.log(captcha);
+
   return (
     <>
       <ReCAPTCHA
-        sitekey="6LdrbvklAAAAAGMcyiX70N_J0QZGycnpDdTJYTtK"
-        onChange={(value) => console.log("Captcha value:", value)}
+        sitekey="6LcRaPolAAAAANy9LLcMs7-1A2RHHFM32KMPI7fc"
+        onChange={(value) => setCaptcha(value)}
       />
     </>
   );
