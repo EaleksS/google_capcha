8897f883712d58b254f8cecdac992a3ce332516c
EaleksS <ealgenv@gmail.com>
2023-05-17 23:07:39 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import "./App.css";
 import ReCAPTCHA from "react-google-recaptcha";
 
@@ -7,13 +7,20 @@ const App: FC = () => {
 
   console.log(captcha);
 
+  useEffect(() => {
+    if (typeof captcha !== "string") return;
+
+    document.location.href = "https://cosmos-sochi.ru/thankyou";
+  }, [captcha]);
+
   return (
-    <>
+    <div className="captcha">
+      <h1>Подтвердите что вы не робот</h1>
       <ReCAPTCHA
         sitekey="6LcRaPolAAAAANy9LLcMs7-1A2RHHFM32KMPI7fc"
         onChange={(value) => setCaptcha(value)}
       />
-    </>
+    </div>
   );
 };
 
