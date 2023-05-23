d6a9656cb1391097b004eab80db8d5c44126d9d7
EaleksS <ealgenv@gmail.com>
2023-05-23 16:52:09 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App: FC = () => {
   useEffect(() => {
     if (typeof captcha !== "string") return;
 
-    document.location.href = "https://cosmos-sochi.ru/thankyou";
+    document.location.href = "https://alean-ak.ru/thankyou";
   }, [captcha]);
 
   return (
